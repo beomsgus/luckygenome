6f3ba57d7e656b587e7ef061643db6a90be4a17b
beomsgus <qjagml2472@gmail.com>
2021-07-06 19:37:43 +0900

    test

---
+++ src/main/java/com/luckygenome/luckygenome/LuckygenomeApplication.java
@@ -0,0 +1,13 @@
+package com.luckygenome.luckygenome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckygenomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckygenomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckygenome/luckygenome/LuckygenomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckygenome.luckygenome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckygenomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
